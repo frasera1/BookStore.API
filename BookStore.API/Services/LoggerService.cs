2f38c75fb111fa90198b25c9aa937df2b2cded0e
Anthony Fraser <ftorrington@hotmail.com>
2020-06-27 19:17:48 -0400

    Add project files.

---
+++ BookStore.API/Services/LoggerService.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.API.Contracts;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        void ILoggerService.LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+
+        void ILoggerService.LogError(string message)
+        {
+            logger.Error(message);
+        }
+
+        void ILoggerService.LogInfo(string message)
+        {
+            logger.Info(message);
+        }
+
+        void ILoggerService.LogWarn(string message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
